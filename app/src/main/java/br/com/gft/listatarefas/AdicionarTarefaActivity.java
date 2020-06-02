b69bb6c32e06f98f2f00fb1c1c2bc51f0ef474ba
leandro-gft <lpsacchi@gmail.com>
2020-06-02 16:24:19 -0300

    Adicionada nova activity para registro da tarefa e configurado FAB para navegação entre activities

---
+++ app/src/main/java/br/com/gft/listatarefas/AdicionarTarefaActivity.java
@@ -0,0 +1,14 @@
+package br.com.gft.listatarefas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionarTarefaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefa);
+    }
+}
