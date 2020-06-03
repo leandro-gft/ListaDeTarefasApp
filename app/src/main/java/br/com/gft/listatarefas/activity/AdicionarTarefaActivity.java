6f58e32fa884f384eba5f312a03467159b81eaae
leandro-gft <lpsacchi@gmail.com>
2020-06-03 09:05:54 -0300

    Adicionado menu

--- app/src/main/java/br/com/gft/listatarefas/activity/AdicionarTarefaActivity.java
+++ app/src/main/java/br/com/gft/listatarefas/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,12 @@
 package br.com.gft.listatarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import br.com.gft.listatarefas.R;
 
@@ -12,5 +16,21 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch ( item.getItemId() ){
+            case R.id.itemSalvar:
+                Toast.makeText(getApplicationContext(),"Você salvou", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
